ddccf43b4c6d1c679497b1b18c99d19ac14c5105
Rogerfps <rpereira40667@ufide.ac.cr>
2026-07-30 14:45:13 -0600

    TallaProveedorError

--- FashionM/FashionM/obj/Debug/net10.0/FashionM.AssemblyInfo.cs
+++ FashionM/FashionM/obj/Debug/net10.0/FashionM.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf7f661bbfd2b4a2272a960fdabeab7cee8e5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec69042ebbafe54d261be90d8fdc48dda612e660")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
